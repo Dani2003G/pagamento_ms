bbeb966343fdb7111009a6e9e9baa24f2c242dff
Dani2003G <daniel0202almeida@outlook.com>
2025-09-11 12:55:08 -0300

    Trocado a criação da fila e alterado para criar uma fanoutExchange

--- src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.pagamentos.amqp;
 
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -14,11 +15,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PagamentoAMQPConfiguration {
 
-    @Bean
-    public Queue criarFila() {
+//    @Bean
+//    public Queue criarFila() {
 //        return new Queue("pagamento.concluido", false);
-        return QueueBuilder.nonDurable("pagamento.concluido").build();
-    }
+//        return QueueBuilder.nonDurable("pagamento.concluido").build();
+//    }
 
     @Bean
     public RabbitAdmin criarRabbbitAdmin(ConnectionFactory conn) {
@@ -40,5 +41,10 @@ public class PagamentoAMQPConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("pagamentos.ex");
     }
 }
